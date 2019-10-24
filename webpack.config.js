a86c63d6d114c11e1ade6de059ccbb1b4290c3e6
Mozart Diniz <mozart.diniz@gamesys.co.uk>
2019-10-24 14:23:30 +0100

    init

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist'
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        VENTURE: JSON.stringify(process.env.VENTURE)
+      }
+    })
+  ]
+};
